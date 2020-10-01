78ead19d8e799a0093c1d7d1a1a598006befe03c
Julian <julian@jgroen.com>
2020-10-01 09:28:27 -0600

    removed autoprefixer plugin, commented out tailwindcss future features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-  purge: {
-      mode: 'all',
-      content: ['./**/**/*.html', './**/**/*.svelte'],
+    purge: {
+        mode: 'all',
+        content: ['./**/**/*.html', './**/**/*.svelte'],
 
-      options: {
-          whitelistPatterns: [/svelte-/],
-          defaultExtractor: (content) => [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
-      },
-  },
+        options: {
+            whitelistPatterns: [/svelte-/],
+            defaultExtractor: (content) =>
+                [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+        },
+    },
 
-  theme: {
-      extend: {},
-  },
-  variants: {},
-  plugins: [],
-  future: {
-      purgeLayersByDefault: true,
-      removeDeprecatedGapUtilities: true,
-  },
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+    future: {
+        // purgeLayersByDefault: true,
+        // removeDeprecatedGapUtilities: true,
+    },
 };
